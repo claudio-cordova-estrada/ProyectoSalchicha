6e04f2ce23521416c6d0d5f22321b5388503477f
scarruiz <sc.ruiz@duocuc.cl>
2023-06-19 22:42:41 -0400

    uuuuu

--- proyectoSalchichaWeb/core/admin.py
+++ proyectoSalchichaWeb/core/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['rut_cliente', 'nombre_cliente', 'email', 'telefono', 'direccion']
+    search_fields = ['rut_cliente', 'nombre_cliente']
+    list_per_page = 15
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'descripcion', 'precio', 'stock', 'imagen']
+    search_fields = ['nombre_producto']
+    list_per_page = 15
+
+
+class BoletaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cliente', 'fecha', 'metodo_pago', 'total' ]
+    list_filter = ['metodo_pago', 'cliente']
+    list_per_page = 15
+
+class DetalleBoletaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'producto', 'boleta', 'cantidad', 'precio_unitario']
+    list_filter = ['producto', 'boleta']
+    list_per_page = 15
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Boleta, BoletaAdmin)
+admin.site.register(DetalleBoleta, DetalleBoletaAdmin)
--- proyectoSalchichaWeb/core/models.py
+++ proyectoSalchichaWeb/core/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
 
 # Create your models here.
+
+class Cliente(models.Model):
+    rut_cliente = models.CharField(max_length=100)
+    nombre_cliente = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.rut_cliente)
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+    imagen = models.ImageField(upload_to='producto')
+
+    def __str__(self):
+        return str(self.nombre)
+
+
+class Boleta(models.Model):
+    cliente = models.ForeignKey('Cliente', on_delete=CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    METODO_PAGO_CHOICES = [
+        ('efectivo', 'Efectivo (pago en local)'),
+        ('debito', 'Tarjeta de débito'),
+        ('credito', 'Tarjeta de crédito'),
+    ]
+    metodo_pago = models.CharField(max_length=30, choices=METODO_PAGO_CHOICES)
+    total = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.id)
+
+
+class DetalleBoleta(models.Model):
+    producto = models.ForeignKey('Producto', on_delete=CASCADE)
+    boleta = models.ForeignKey('Boleta', on_delete=CASCADE)
+    cantidad = models.PositiveIntegerField()
+    precio_unitario = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.boleta)
